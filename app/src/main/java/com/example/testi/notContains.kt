663378e4441b6a64dff5ddc39655abcb53f8f14f
Merab Kochlamazashvili <meko123mekonom@gmail.com>
2022-07-02 00:27:01 +0400

    123456

--- app/src/main/java/com/example/testi/notContains.kt
+++ app/src/main/java/com/example/testi/notContains.kt
@@ -11,7 +11,7 @@ private fun notContains(x: Array<Int>, n: Int): Int  {
     for (i in 0 until n) {
         mySet.add(x[i])
     }
-    //val it = mySet.iterator()
+
     while (mySet.isNotEmpty()){
         if (z != mySet.first()) {
             println(z)
